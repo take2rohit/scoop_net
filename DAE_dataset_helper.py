e76319cf7787bd94e9ac772999f47aaa43679149
Rohit Lal <rohitlal789@gmail.com>
2020-06-08 12:15:02 +0530

    added CNN based DAE

---
+++ DAE_dataset_helper.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np,os
+
+class OrigamiDatasetGenerate(Dataset):
+
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.all_image_filenames = [f for f in os.listdir(root_dir) if os.path.isfile(os.path.join(root_dir, f))]
+
+    def __len__(self):
+        return len(self.all_image_filenames)
+    
+    def image_augmentation(self, image, noise_typ = "s&p"):
+        if noise_typ == "gauss":
+            row,col,ch= image.shape
+            mean = 0
+            var = 0
+            sigma = var**0.5
+            gauss = np.random.normal(mean,sigma,(row,col,ch))
+            gauss = gauss.reshape(row,col,ch)
+            noisy = image + gauss
+            
+            return noisy
+
+        elif noise_typ == "s&p":
+            row,col,ch = image.shape
+            s_vs_p = 0.1
+            amount = 0.04
+            out = np.copy(image)
+            # Salt mode
+            num_salt = np.ceil(amount * image.size * s_vs_p)
+            coords = [np.random.randint(0, i - 1, int(num_salt))
+                  for i in image.shape]
+            out[coords] = 1
+
+            # Pepper mode
+            num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+            coords = [np.random.randint(0, i - 1, int(num_pepper))
+                  for i in image.shape]
+            out[coords] = 0
+            return out    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = os.path.join(self.root_dir,
+                                self.all_image_filenames[idx])
+        image = plt.imread(img_path)
+        
+        augmented = self.image_augmentation(image, noise_typ = "gauss")
+        sample = {'augmented': augmented, 'original': image}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+    
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        original, augmented = sample['original'], sample['augmented']
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        original = np.transpose(original, (2, 0, 1))
+        augmented = np.transpose(augmented, (2, 0, 1))
+        return {'original': torch.from_numpy(original).float(),
+                'augmented': torch.from_numpy(augmented).float()}
+
+class Grayscale(object):
+    """Convert ndarrays in sample to 2D."""
+
+    def __call__(self, sample):
+        original, augmented = sample['original'], sample['augmented']
+        original = torch.mean(original, dim=0)
+        augmented = torch.mean(augmented, dim=0)
+        return {'original': original, 'augmented': augmented}
+
+class Resize(object):
+    """ndarrays resize."""
+    
+    def __init__(self, size):
+        self.size = size
+        
+    def __call__(self, sample):
+        original, augmented = sample['original'], sample['augmented']
+        original = cv2.resize(original, dsize=self.size, interpolation=cv2.INTER_CUBIC)
+        augmented = cv2.resize(augmented, dsize=self.size, interpolation=cv2.INTER_CUBIC)
+        return {'original': original, 'augmented':augmented }
+
+class Normalize(object):
+    
+    def __call__(self, sample):
+        original, augmented = sample['original'], sample['augmented']
+        
+        min_noisy,max_noisy = np.min(original),np.max(original)
+        original = (original - min_noisy)/(max_noisy-min_noisy)
+        
+        min_noisy,max_noisy = np.min(augmented),np.max(augmented)
+        augmented = (augmented - min_noisy)/(max_noisy-min_noisy)
+        return {'original': original, 'augmented':augmented }
+
+class RandomBackground(object):
+
+    def __init__(self, bg_folder):
+        self.root_dir = bg_folder
+    
+    def __call__(self, sample):
+        original, augmented = np.rint(sample['original']*255), sample['augmented']
+        original = original.astype(np.uint8)
+        bg_image_filenames = [f for f in os.listdir(self.root_dir) if os.path.isfile(os.path.join(self.root_dir, f))]
+
+        i = np.random.randint(0,len(bg_image_filenames))
+        bg = cv2.imread(os.path.join(self.root_dir,bg_image_filenames[i]))
+
+        req_sh = original.shape
+        bg = cv2.resize(bg, dsize=(req_sh[0],req_sh[1]))
+        
+        gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+        # ret2,mask = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        ret2,mask = cv2.threshold(gray,50,255,cv2.THRESH_BINARY_INV)
+        
+        new= cv2.bitwise_and(bg,bg, mask= mask)
+        new= cv2.bitwise_or(original,new)
+        
+        new = cv2.cvtColor(new,cv2.COLOR_BGR2RGB)
+        return {'original': original/255, 'augmented':new/255 }
---
+++ DAE_model.py
@@ -0,0 +1,94 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AugmentedAutoencoder(nn.Module):
+    def __init__(self):
+        super(AugmentedAutoencoder, self).__init__()
+        # nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros')
+        self.en_conv1 = nn.Conv2d(3, 512, 2, stride=2) 
+        self.en_conv2 = nn.Conv2d(512, 256, 2, stride=2)
+        self.en_conv3 = nn.Conv2d(256, 256, 2, stride=2)
+        self.en_conv4 = nn.Conv2d(256, 512, 2, stride=2)
+        
+        self.fc1 = nn.Linear(512*8*8, 2048)
+        self.fc2 = nn.Linear(2048, 512*8*8)
+        
+        self.dc_conv2 = torch.nn.ConvTranspose2d(512,256,2, stride=2)
+        self.dc_conv3 = torch.nn.ConvTranspose2d(256, 256, 2, stride=2)
+        self.dc_conv4 = torch.nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.dc_conv5 = torch.nn.ConvTranspose2d(128, 3, 2, stride=2)
+
+    def forward(self, x):
+        x = self.en_conv1(x) # N x 128 x 64 x 64
+        x = F.relu(x) 
+        
+        x = self.en_conv2(x) # N x 256 x 32 x 32
+        x = F.relu(x)
+        
+        x = self.en_conv3(x) # N x 256 x 16 x 16
+        x = F.relu(x)
+        
+        x = self.en_conv4(x) # N x 512 x 8 x 8
+        x = F.relu(x)
+        
+        
+        x = torch.flatten(x, 1) # N x (512*8*8)       
+        encoding = self.fc1(x) # N x 128        
+        x = self.fc2(encoding) # N x (512*8*8)
+        x = x.view(-1,512,8,8)
+
+
+        x = self.dc_conv2(x) # N x 256 x 16 x 16
+        x = F.relu(x)
+
+        x = self.dc_conv3(x) # N x 256 x 32 x 32
+        x = F.relu(x)
+        
+        x = self.dc_conv4(x) # N x 256 x 64 x 64
+        x = F.relu(x)
+
+        x = self.dc_conv5(x) # N x 3 x 128 x 128
+        x = F.sigmoid(x)
+
+        return x
+
+class DenoisingAE(nn.Module):
+    def __init__(self):
+        super(DenoisingAE,self).__init__()
+        self.ec1 = nn.Linear(100*100, 512)
+        self.ec2 = nn.Linear(512, 256)
+        self.ec3 = nn.Linear(256, 128)
+        self.ec4 = nn.Linear(128, 64)
+        
+        self.dc1 = nn.Linear(64, 128)
+        self.dc2 = nn.Linear(128, 256)
+        self.dc3 = nn.Linear(256, 512)
+        self.dc4 = nn.Linear(512, 100*100)
+
+    def forward(self, x):
+        orig_size =  x.shape
+        x = x.view(orig_size[0], -1).float()
+        x = self.ec1(x)
+        x = F.relu(x)
+        x = self.ec2(x)
+        x = F.relu(x)
+        x = self.ec3(x)
+        x = F.relu(x)
+        x = self.ec4(x)
+        encoded = F.relu(x)
+
+        x = self.dc1(encoded)
+        x = F.relu(x)
+        x = self.dc2(x)
+        x = F.relu(x)
+        x = self.dc3(x)
+        x = F.relu(x)
+        x = self.dc4(x)
+        reconst = F.sigmoid(x)
+        return reconst
+
+# model = AugmentedAutoencoder()
+# t = torch.rand([64, 3, 128, 128])
+# model.forward(t).shape
